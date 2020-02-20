dffae0e33dea5394e53eec6beb70f02bbe236da6
Ethan Irvine <edog.irvine@gmail.com>
2020-02-19 23:12:00 -0700

    connected to the database

---
+++ assets/connect.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employeeDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+//   runSearch();
+    console.log("you are connected");
+});
