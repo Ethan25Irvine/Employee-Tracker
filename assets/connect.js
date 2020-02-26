9d8485bcf2470e8710a7a24a89abeab9095b7939
Ethan Irvine <edog.irvine@gmail.com>
2020-02-26 14:59:05 -0700

    finished the add functions

--- assets/connect.js
+++ assets/connect.js
@@ -26,6 +26,8 @@ connection.connect(function(err) {
 
 let role = [];
 let managers = [];
+let department = [];
+ 
 
 function runSearch() {
     inquirer
@@ -57,11 +59,11 @@ function runSearch() {
           break;
   
         case "Add department":
-          departmentCreate();
+          addDepartment();
           break;
   
         case "Add role":
-          departmentCreate();
+          addRole();
           break;
 
         case "Add employee":
@@ -85,7 +87,7 @@ function departmentSearch(){
 function roleSearch(){
     const query = "SELECT * FROM role";
     connection.query( query, function(err, res) {
-        console.log(res);
+        // console.log(res);
         for (let i = 0; i < res.length; i++) {
         console.log("Role: " + res[i].title + " || ID: " + res[i].id + "|| Salary: " + res[i].salary + " || Department ID: " + res[i].department_id);
         }
@@ -120,13 +122,13 @@ function addEmployee(){
             message: "What is the employees last name?"
         },
         {
-            name: "role",
+            name: "role_id",
             type:"rawlist",  
             message: "What role does this employee have?",
             choices: role
         },
         {
-            name: "manager",
+            name: "manager_id",
             type: "rawlist",
             message: "Who is their manager",
             choices: managers
@@ -134,39 +136,143 @@ function addEmployee(){
     ])
     .then (function (answer){
         console.log(answer);
-        // connection.query("INSERT INTO employee")
+        // connection.query(
+        //     "INSERT INTO employee SET ?",
+        //     {
+        //       first_name: answer.first_name,
+        //       last_name: answer.last_name,
+        //       role_id: answer.role_id,
+        //       manager_id: answer.manager_id
+        //     },
+        //     function(err) {
+        //       if (err) throw err;
+        //       console.log("Your employee was added successfully!");
+        //       runSearch();
+        //     }
+        //   );
     })
     
+};
+
+function addDepartment(){
+    
+        
+    inquirer
+    .prompt ([
+        {
+            name: "name",
+            type: "input",
+            message: "What department do you want to add?"
+        },
+    ])
+    .then (function (answer){
+        console.log(answer);
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+              name: answer.name
+            },
+            function(err) {
+              if (err) throw err;
+              console.log("Your employee was added successfully!");
+              runSearch();
+            }
+          );
+    })
+};
+
+function addRole(){
+    
+        
+    inquirer
+    .prompt ([
+        {
+            name: "title",
+            type: "input",
+            message: "What role do you want to add?"
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "How much money does this role make?" 
+        },
+        {
+            name: "department_id",
+            type: "rawlist",
+            message: "What Department does this role work under?",
+            choices: department
+        }
+    ])
+    .then (function (answer){
+        console.log(answer);
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.title,
+              salary: parseInt(answer.salary),
+              department_id: answer.department_id
+            },
+            function(err) {
+              if (err) throw err;
+              console.log("Your employee was added successfully!");
+              runSearch();
+            }
+          );
+    })
 };
 
 async function getRoles() {
 
 
-    let query = 'SELECT * FROM role'
+    const query = 'SELECT * FROM role'
   
-    role = await connection.query(query, function (){
-        ({
-            name: title,
-            value: id
-        })
-    })
+    await connection.query(query, function (err, res){
+        if (err) throw err;
+        console.log(res);
+        for (let i = 0; i < res.length; i++){
+                role.push({
+                name: res[i].title,
+                value: res[i].id
+            });
+        }
+        // console.log(role);
+    });
 
-   
-     
+        
+        await connection.query("SELECT * FROM department", function(err, res){
+            if (err) throw err;
+            console.log(res);
+            for (let i = 0; i<res.length; i++){
+                
+                department.push({
+                    name: res[i].name,
+                    department_id: res[i].id
+                })          
+            }
+            // console.log(department);
+        });
+
     const newQuery = 'SELECT * FROM employee'
 
-    managers = await connection.query(newQuery, function () {
-        (
-            {
-                name: first_name + " " + last_name,
-                value: id
+    await connection.query(newQuery, function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+
+        for (let i = 0; i<res.length; i++){
+            managers.push({
+                name: res[i].first_name + " " + res[i].last_name,
+                value: res[i].id
             },
-            {
+            )
+        }
+        // console.log(managers);
+           managers.push({
                 name: "no manager",
                 value: null
-            }
-        )
-    })
+            });
+        
+    });
+
     runSearch();
 }
 
---
+++ assets/schema.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE employeeDB; 
+USE employeeDB;
+CREATE TABLE department (
+	id  INT AUTO_INCREMENT,
+	name  VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL (10,4),
+	department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT,
+	manager_id INT,
+    PRIMARY KEY (id)
+);
